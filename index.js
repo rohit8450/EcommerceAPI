c3cf2539fb4bdd034602cc9c7ce28fff75c17536
rohit8450 <rohitrajthakur10@gmail.com>
2023-02-11 23:33:31 +0530

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const db = require("./config/mongoose");
 const PORT = process.env.PORT || 8000;
 dotenv.config();
 const app = express();
-
-
-mongoose.connect(process.env.MONGO_DB_URL)
-        .then(() => console.log("Mongo DB Connection is Successful"))
-        .catch((err) => {
-            console.log(err);
-    });
 
 // All API route goes here
 app.use(express.json());
